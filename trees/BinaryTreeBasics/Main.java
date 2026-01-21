a3c0524cef8edf8e3ab7c08af0eb03ec6aa2fe72
Shankar-794 <shankar9666188@gmail.com>
2026-01-21 11:53:47 +0530

    Started Binary Trees: basic node structure and manual tree creation

--- trees/BinaryTreeBasics/Main.java
+++ trees/BinaryTreeBasics/Main.java
@@ -1,1 +1,28 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
 
+public class Main{
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        /*
+                1
+               / \
+              2   3
+             / \
+            4   5
+        */
+       System.out.println("Binary Tree created successfully");
+    }
+}
