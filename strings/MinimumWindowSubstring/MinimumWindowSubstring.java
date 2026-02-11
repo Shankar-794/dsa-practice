d291fd8e64cc6e1f50b503c3ef82f56cfb90814f
Shankar-794 <shankar9666188@gmail.com>
2026-02-11 20:36:31 +0530

    Solved minimum window substring using advanced sliding window technique

---
+++ strings/MinimumWindowSubstring/MinimumWindowSubstring.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class MinimumWindowSubstring {
+
+    static String minWindow(String s, String t) {
+
+        if (s.length() < t.length())
+            return "";
+
+        int[] need = new int[128];
+        int required = 0;
+
+        for (char c : t.toCharArray()) {
+            if (need[c] == 0)
+                required++;
+            need[c]++;
+        }
+
+        int[] window = new int[128];
+        int formed = 0;
+        int left = 0;
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+            window[c]++;
+
+            if (window[c] == need[c])
+                formed++;
+
+            while (formed == required) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                window[leftChar]--;
+
+                if (window[leftChar] < need[leftChar])
+                    formed--;
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+    }
+}
