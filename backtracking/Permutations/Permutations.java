f805f640de9b826340d40c7b6a53e8db33a41615
Shankar-794 <shankar9666188@gmail.com>
2026-02-16 19:52:32 +0530

    Implemented permutations using backtracking with used array

---
+++ backtracking/Permutations/Permutations.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Permutations {
+
+    static List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), result);
+        return result;
+    }
+
+    static void backtrack(int[] nums, boolean[] used,
+                          List<Integer> current,
+                          List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i])
+                continue;
+
+            // choose
+            used[i] = true;
+            current.add(nums[i]);
+
+            // explore
+            backtrack(nums, used, current, result);
+
+            // un-choose
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = permute(nums);
+
+        for (List<Integer> perm : result) {
+            System.out.println(perm);
+        }
+    }
+}
